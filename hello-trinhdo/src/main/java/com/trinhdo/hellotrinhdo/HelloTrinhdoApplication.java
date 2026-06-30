1157eb9778a23a9a0e478ad85755359e29736385
Nguyen Cong Trinh Do <nguyencongdo20@gmail.com>
2026-07-01 00:42:38 +0700

    add new project and test api

---
+++ hello-trinhdo/src/main/java/com/trinhdo/hellotrinhdo/HelloTrinhdoApplication.java
@@ -0,0 +1,13 @@
+package com.trinhdo.hellotrinhdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTrinhdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTrinhdoApplication.class, args);
+	}
+
+}
---
+++ hello-trinhdo/src/test/java/com/trinhdo/hellotrinhdo/HelloTrinhdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhdo.hellotrinhdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTrinhdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
