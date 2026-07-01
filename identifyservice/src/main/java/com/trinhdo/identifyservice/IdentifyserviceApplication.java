138b8be0501069b922a9ef7bfe47b4523dde5414
congdo20 <nguyencongdo20@gmail.com>
2026-07-01 21:26:58 +0700

    add project connect database in spring

---
+++ identifyservice/src/main/java/com/trinhdo/identifyservice/IdentifyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.trinhdo.identifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentifyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentifyserviceApplication.class, args);
+	}
+
+}
---
+++ identifyservice/src/test/java/com/trinhdo/identifyservice/IdentifyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhdo.identifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentifyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
